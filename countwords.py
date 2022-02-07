155f436097a6567c97c95652884040e46e984051
Emanuel Elias <eng.emanuel.elias@gmail.com>
2022-02-07 16:53:00 -0300

    Add files via upload

---
+++ countwords.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+from nltk.tokenize import RegexpTokenizer
+
+tokenizer = RegexpTokenizer(r'\w+')
+text = "This is my text. It icludes commas, question marks? and other stuff. Also U.S.."
+tokens = tokenizer.tokenize(text)
+
+for i xrange(len(tokens)):
+    i+1
+"""
+print ("Leu o arquivo .py")
